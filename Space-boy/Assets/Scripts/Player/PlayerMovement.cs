76bc37b7d47a0b02276e1dc5581c00daa3967e5e
Tycho van <TychovanEngelenburg@gmail.com>
2021-04-08 13:35:43 +0200

    .
    	.

--- Space-boy/Assets/Scripts/Player/PlayerMovement.cs
+++ Space-boy/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,14 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement Settings")]
     [Range(0, 15)] public float startSpeed = 10f;
-    [Range(0, 15)] public float vDrag = 5f;
+    [Range(0, 15)] public float vDrag = 3f;
+    public float bank = 50f;
 
     [SerializeField] private bool canMove = true;
     private float currentSpeedH;
     private float currentSpeedV;
+    private Rigidbody rb;
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
         currentSpeedH = startSpeed;
         currentSpeedV = startSpeed;
     }
@@ -38,7 +41,13 @@ public class PlayerMovement : MonoBehaviour
         //If the user can move, it will move according to the current input given.
         if (canMove)
         {
-            transform.position += new Vector3(Mathf.Sin(Hor * currentSpeedH), 0, Mathf.Sin(Ver * currentSpeedV));
+            rb.transform.position += new Vector3(Mathf.Sin(Hor * currentSpeedH), 0, Mathf.Sin(Ver * currentSpeedV));
+            if (Hor != 0)
+            {
+                rb.transform.eulerAngles = new Vector3(rb.transform.eulerAngles.x, rb.transform.eulerAngles.y + 50, rb.transform.eulerAngles.z);
+                //rb.transform.Rotate(0, 0, Mathf.Sin(-Hor * bank));
+            }            
+            
         }
 
 

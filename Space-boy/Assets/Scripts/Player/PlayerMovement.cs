e760f04ebd8c9e37e9efa7bd2d6016e955a0add7
Tycho van <TychovanEngelenburg@gmail.com>
2021-04-08 12:07:49 +0200

    Movement input changes
    Movement now works using a sinus wave. Also there is a vertical drag which will make the player go slower forward and faster backward.
    
    On top of this I cleaned up the movement settings a bit. Making it more accesible and easy to work with.

--- Space-boy/Assets/Scripts/Player/PlayerMovement.cs
+++ Space-boy/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,22 +4,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private bool canMove = true;
-    [SerializeField] private float speed = 10;
+    [Header("Movement Settings")]
+    [Range(0, 15)] public float startSpeed = 10f;
+    [Range(0, 15)] public float vDrag = 5f;
 
-    void Update()
+    [SerializeField] private bool canMove = true;
+    private float currentSpeedH;
+    private float currentSpeedV;
+
+    private void Start()
     {
+        currentSpeedH = startSpeed;
+        currentSpeedV = startSpeed;
+    }
+
+    private void Update()
+    {
+        //Input detection
         float Hor = Input.GetAxis("Horizontal") * Time.deltaTime;
         float Ver = Input.GetAxis("Vertical") * Time.deltaTime;
 
+        //This implements drag to the spaceship/player. 
+        //The spaceship is flying fast, meaning its harder to go faster and you slow down easier.
+        if (Ver > 0)
+        {
+            currentSpeedV = startSpeed - vDrag;
+        }
+        else if (Ver < 0)
+        {
+            currentSpeedV = startSpeed + vDrag;
+        }
+    
+        //If the user can move, it will move according to the current input given.
         if (canMove)
         {
-            transform.position += new Vector3(Hor * speed, 0, Ver * speed);
+            transform.position += new Vector3(Mathf.Sin(Hor * currentSpeedH), 0, Mathf.Sin(Ver * currentSpeedV));
         }
 
+
+
+        /*      Possible dash function (by Sten)
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            transform.position += new Vector3(Hor * speed * 50, 0, Ver * speed * 50);
+            transform.position += new Vector3( Hor * speed * 50, 0, Ver * speed * 50);
         }
+        */
     }
+
 }
